cbcfeeb92388f4d17ab7ad9b72293f93c52caa90
Brie Morris <morris.brie@gmail.com>
2023-07-18 15:32:39 -0500

    added css

---
+++ client.js
@@ -0,0 +1,35 @@
+let firstName = '';
+let lastName = '';
+let idNumber = 0;
+let title = '';
+let annualSalary = 0;
+
+function handleSubmit(event){
+  event.preventDefault();
+  let firstVal = document.querySelector('#first-name').value;
+  console.log(firstVal);
+  let lastVal = document.querySelector('#last-name').value;
+  console.log(lastVal);
+  let idVal = document.querySelector('#id-number').value;
+  console.log(idVal);
+  let titleVal = document.querySelector('#job-title').value;
+  console.log(titleVal);
+  let salaryVal = document.querySelector('#annual-salary').value;
+  console.log(salaryVal);
+
+  let employeeTable = document.querySelector('#employees-info');
+  employeeTable.innerHTML +=`
+    <tr>
+      <td>${firstVal}</td>
+      <td>${lastVal}</td>
+      <td>${idVal}</td>
+      <td>${titleVal}</td>
+      <td>${salaryVal}</td>
+      <td><button onclick="removeRow(event)">Delete</button></td>
+   </tr>
+  `;
+
+}
+//write remove row function
+
+
